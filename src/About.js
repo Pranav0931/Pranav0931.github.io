c7193ad13ecfe9cd1dd0120128b6aac52a527057
Pranav0931 <164500130+Pranav0931@users.noreply.github.com>
2024-04-08 23:17:24 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const About = () => {
+ return (
+ <div>
+ <h2>About Us</h2>
+ <p>Learn more about our company and our mission.</p>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ </div>
+ );
+}
+export default About;
