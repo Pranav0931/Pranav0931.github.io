75663238bcd1990f522f5ecbaa652bf83836acef
Pranav0931 <164500130+Pranav0931@users.noreply.github.com>
2024-04-08 22:36:27 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const Contact = () => {
+ return (
+ <div>
+ <h2>Contact Us</h2>
+ <p>Reach out to us for any inquiries or feedback.</p>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ </div>
+ );
+}
+export default Contact;
