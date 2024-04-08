c72f2aa1e8487258d2491ee04afd8b875f4f20d7
Pranav0931 <164500130+Pranav0931@users.noreply.github.com>
2024-04-08 23:21:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 const App = () => {
  return (
  <Router>
